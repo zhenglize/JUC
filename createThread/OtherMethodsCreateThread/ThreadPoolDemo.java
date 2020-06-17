3c5712b3e751a116abb7e89bb4a4aef1d260104e
zhenglize <zhenglizeA@126.com>
2020-06-17 17:09:40 +0800

    使用线程池来创建线程

---
+++ createThread/OtherMethodsCreateThread/ThreadPoolDemo.java
@@ -0,0 +1,31 @@
+package createThread.OtherMethodsCreateThread;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+*@author zhenglize
+  使用线程池来创建线程
+*/
+public class ThreadPoolDemo {
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        for (int i = 0; i < 1000; i++) {
+            executorService.submit(new Task() {
+            });
+        }
+    }
+}
+
+class Task implements Runnable {
+
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(Thread.currentThread().getName());
+    }
+}
